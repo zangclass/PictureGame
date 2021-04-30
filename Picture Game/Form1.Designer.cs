1b86eb54c52c3e09c01089c4dbdb437cc6f77d85
Ebrahim <Ebrahim@DESKTOP-5ND39I7>
2021-05-01 01:00:02 +0430

    تگ های دکمه خودمون رو ست کردیم از شماره عکس

--- Picture Game/Form1.Designer.cs
+++ Picture Game/Form1.Designer.cs
@@ -28,13 +28,318 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PanelBoarde = new System.Windows.Forms.Panel();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn14 = new System.Windows.Forms.Button();
+            this.btn15 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn17 = new System.Windows.Forms.Button();
+            this.btn18 = new System.Windows.Forms.Button();
+            this.btn19 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn16 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.PanelBoarde.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCyan;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 340);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(781, 70);
+            this.panel1.TabIndex = 0;
+            // 
+            // PanelBoarde
+            // 
+            this.PanelBoarde.Controls.Add(this.listBox1);
+            this.PanelBoarde.Controls.Add(this.btn17);
+            this.PanelBoarde.Controls.Add(this.btn18);
+            this.PanelBoarde.Controls.Add(this.btn19);
+            this.PanelBoarde.Controls.Add(this.btn20);
+            this.PanelBoarde.Controls.Add(this.btn16);
+            this.PanelBoarde.Controls.Add(this.btn12);
+            this.PanelBoarde.Controls.Add(this.btn13);
+            this.PanelBoarde.Controls.Add(this.btn14);
+            this.PanelBoarde.Controls.Add(this.btn15);
+            this.PanelBoarde.Controls.Add(this.btn11);
+            this.PanelBoarde.Controls.Add(this.btn7);
+            this.PanelBoarde.Controls.Add(this.btn8);
+            this.PanelBoarde.Controls.Add(this.btn9);
+            this.PanelBoarde.Controls.Add(this.btn10);
+            this.PanelBoarde.Controls.Add(this.btn6);
+            this.PanelBoarde.Controls.Add(this.btn2);
+            this.PanelBoarde.Controls.Add(this.btn3);
+            this.PanelBoarde.Controls.Add(this.btn4);
+            this.PanelBoarde.Controls.Add(this.btn5);
+            this.PanelBoarde.Controls.Add(this.btn1);
+            this.PanelBoarde.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelBoarde.Location = new System.Drawing.Point(0, 0);
+            this.PanelBoarde.Name = "PanelBoarde";
+            this.PanelBoarde.Size = new System.Drawing.Size(781, 340);
+            this.PanelBoarde.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn1.Location = new System.Drawing.Point(45, 43);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(70, 61);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn5.Location = new System.Drawing.Point(349, 43);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(70, 61);
+            this.btn5.TabIndex = 2;
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn4.Location = new System.Drawing.Point(273, 43);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(70, 61);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn3.Location = new System.Drawing.Point(197, 43);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(70, 61);
+            this.btn3.TabIndex = 4;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn2.Location = new System.Drawing.Point(121, 43);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(70, 61);
+            this.btn2.TabIndex = 5;
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn7.Location = new System.Drawing.Point(121, 110);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(70, 61);
+            this.btn7.TabIndex = 11;
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn8.Location = new System.Drawing.Point(197, 110);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(70, 61);
+            this.btn8.TabIndex = 10;
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn9.Location = new System.Drawing.Point(273, 110);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(70, 61);
+            this.btn9.TabIndex = 9;
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn10.Location = new System.Drawing.Point(349, 110);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(70, 61);
+            this.btn10.TabIndex = 8;
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.btn10_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn6.Location = new System.Drawing.Point(45, 110);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(70, 61);
+            this.btn6.TabIndex = 6;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn12.Location = new System.Drawing.Point(121, 177);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(70, 61);
+            this.btn12.TabIndex = 17;
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.btn12_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn13.Location = new System.Drawing.Point(197, 177);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(70, 61);
+            this.btn13.TabIndex = 16;
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.btn13_Click);
+            // 
+            // btn14
+            // 
+            this.btn14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn14.Location = new System.Drawing.Point(273, 177);
+            this.btn14.Name = "btn14";
+            this.btn14.Size = new System.Drawing.Size(70, 61);
+            this.btn14.TabIndex = 15;
+            this.btn14.UseVisualStyleBackColor = true;
+            this.btn14.Click += new System.EventHandler(this.btn14_Click);
+            // 
+            // btn15
+            // 
+            this.btn15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn15.Location = new System.Drawing.Point(349, 177);
+            this.btn15.Name = "btn15";
+            this.btn15.Size = new System.Drawing.Size(70, 61);
+            this.btn15.TabIndex = 14;
+            this.btn15.UseVisualStyleBackColor = true;
+            this.btn15.Click += new System.EventHandler(this.btn15_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn11.Location = new System.Drawing.Point(45, 177);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(70, 61);
+            this.btn11.TabIndex = 12;
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.btn11_Click);
+            // 
+            // btn17
+            // 
+            this.btn17.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn17.Location = new System.Drawing.Point(121, 244);
+            this.btn17.Name = "btn17";
+            this.btn17.Size = new System.Drawing.Size(70, 61);
+            this.btn17.TabIndex = 23;
+            this.btn17.UseVisualStyleBackColor = true;
+            this.btn17.Click += new System.EventHandler(this.btn17_Click);
+            // 
+            // btn18
+            // 
+            this.btn18.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn18.Location = new System.Drawing.Point(197, 244);
+            this.btn18.Name = "btn18";
+            this.btn18.Size = new System.Drawing.Size(70, 61);
+            this.btn18.TabIndex = 22;
+            this.btn18.UseVisualStyleBackColor = true;
+            this.btn18.Click += new System.EventHandler(this.btn18_Click);
+            // 
+            // btn19
+            // 
+            this.btn19.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn19.Location = new System.Drawing.Point(273, 244);
+            this.btn19.Name = "btn19";
+            this.btn19.Size = new System.Drawing.Size(70, 61);
+            this.btn19.TabIndex = 21;
+            this.btn19.UseVisualStyleBackColor = true;
+            this.btn19.Click += new System.EventHandler(this.btn19_Click);
+            // 
+            // btn20
+            // 
+            this.btn20.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn20.Location = new System.Drawing.Point(349, 244);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(70, 61);
+            this.btn20.TabIndex = 20;
+            this.btn20.UseVisualStyleBackColor = true;
+            this.btn20.Click += new System.EventHandler(this.btn20_Click);
+            // 
+            // btn16
+            // 
+            this.btn16.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn16.Location = new System.Drawing.Point(45, 244);
+            this.btn16.Name = "btn16";
+            this.btn16.Size = new System.Drawing.Size(70, 61);
+            this.btn16.TabIndex = 18;
+            this.btn16.UseVisualStyleBackColor = true;
+            this.btn16.Click += new System.EventHandler(this.btn16_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(497, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(190, 212);
+            this.listBox1.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(781, 410);
+            this.Controls.Add(this.PanelBoarde);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PanelBoarde.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel PanelBoarde;
+        private System.Windows.Forms.Button btn17;
+        private System.Windows.Forms.Button btn18;
+        private System.Windows.Forms.Button btn19;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn16;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn14;
+        private System.Windows.Forms.Button btn15;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Picture Game/Form1.cs
+++ Picture Game/Form1.cs
@@ -16,5 +16,167 @@ namespace Picture_Game
         {
             InitializeComponent();
         }
+        private Button SearchButton(string buttonName)
+        {
+            foreach (var item in PanelBoarde.Controls)
+            {
+                if (item.GetType() == typeof(Button))
+                {
+                    var obj = item as Button;
+                    if (obj.Name == buttonName)
+                    {
+                        return obj;
+                    }
+                }
+            }
+            return null;
+        }
+        private void SetButtonPicture()
+        {
+            int picCount = 1, setPicCounter = 0;
+            while (picCount < 11)
+            {
+                Random rand = new Random();
+                var generateRandomNumber = rand.Next(1, 21);
+                var btnSearch = SearchButton("btn" + generateRandomNumber);
+                if (btnSearch.Tag == null)
+                {
+                    btnSearch.Tag = picCount;
+
+                    setPicCounter++;
+                    if(setPicCounter == 2)
+                    {
+                        setPicCounter = 0;
+                        picCount++;
+                    }
+                }
+
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetButtonPicture();
+
+            foreach (var item in PanelBoarde.Controls)
+            {
+                if (item.GetType() == typeof(Button))
+                {
+                    var obj = item as Button;
+                    if (obj.Tag != null)
+                    {
+                        listBox1.Items.Add(obj.Tag.ToString());
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Null");
+                    }
+                }
+            }
+        }
+
+        private void Checkplay(Button btn)
+        {
+            MessageBox.Show(btn.Tag.ToString()); ;
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn1);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn2);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn3);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn4);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn5);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn7);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn8);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn9);
+        }
+
+        private void btn10_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn10);
+        }
+
+        private void btn11_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn11);
+        }
+
+        private void btn12_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn12);
+        }
+
+        private void btn13_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn13);
+        }
+
+        private void btn14_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn14);
+        }
+
+        private void btn15_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn15);
+        }
+
+        private void btn16_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn16);
+        }
+
+        private void btn17_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn17);
+        }
+
+        private void btn18_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn18);
+        }
+
+        private void btn19_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn19);
+        }
+
+        private void btn20_Click(object sender, EventArgs e)
+        {
+            Checkplay(btn20);
+        }
     }
 }
